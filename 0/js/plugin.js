4e531b9c8330ee7b25cba9c6f9f6d6e74b2e88e6
Navruzbek2103 <navrozbek2306@gmail.com>
2022-11-25 22:59:21 +0500

    Full app

--- 0/js/plugin.js
+++
@@ -1,6 +0,0 @@
-let getElementSingle = function (selector){
-  return document.querySelector(selector)
-}
-let getElementSingleq = function (selector){
-  return document.querySelectorAll(selector)
-}
--- js/main.js
+++ js/main.js
@@ -14,7 +14,12 @@ setTimeout(() => {
   //   console.log(e);
   // })
 
+  // LEFT:  left: 223px; top: 206px
+  // RIGHT: left: 916px; top: 206px;
+  let finish = new Audio("./../audio/finish.ogg")
   $("body").addEventListener("mousedown", (e) =>{
+    $("#button1").style.transition = "all 0s ease-in"
+    $("#button2").style.transition = "all 0s ease-in"
     $("#button1").style.position = "absolute"
     
     if(e.target == $("#button1")){
@@ -27,6 +32,8 @@ setTimeout(() => {
         locationEqual.style.left = `${x - 25}px`;
         locationEqual.style.top = `${y - 25}px`;
       })
+
+      
     }
     else if(e.target == $("#button2")){
   
@@ -42,7 +49,57 @@ setTimeout(() => {
   })
   
   document.addEventListener("mouseup", () => {
-    locationEqual = 0;
-    locationNotEqual = 0;
-  })
+        locationEqual = 0;
+        locationNotEqual = 0;
+        // left: left: 406
+        // left: top: 206
+
+        // right: left: 916
+        // right: top: 206
+
+        // btnLeft : left: 628
+        // btnLeft : top: 378
+
+        // btnRight : left: 685
+        // btnRight : top: 378
+        if($("#button1").style.left > '374px' && $("#button1").style.left < '436px' && $("#button1").style.top > '174px' && $("#button1").style.top < '236px'){
+          $("#button1").style.transition = "all .4s ease-in";
+          $("#button1").style.left = "406px";
+          $("#button1").style.top = "206px";
+          $("#button1").style.border = "3px solid #98BB88";
+        }
+
+        else{
+          $("#button1").style.left = '628px';
+          $("#button1").style.top = '378px';
+          $("#button1").style.transition = 'all .4s ease-in';
+          $("#button1").style.border = '3px solid #59C2E2';
+        }
+        if($("#button2").style.left > "886px" && $("#button2").style.left < "946px" && $("#button2").style.top > "176px" && $("#button2").style.top < "236px"){
+          $("#button2").style.left = "916px";
+          $("#button2").style.top = "206px";
+          $("#button2").style.transition = "all .4s ease-in";
+          $("#button2").style.border = "3px solid #98BB88";
+        }
+        else{
+          $("#button2").style.left = "685px";
+          $("#button2").style.top = "378px";
+          $("#button2").style.transition = "all .4s ease-in";
+          $("#button2").style.border = "3px solid #59C2E2";
+        }
+
+        (function(){
+          if($("#button1").style.left == '406px' && $("#button1").style.top == "206px" && $("#button2").style.left == "916px" && $("#button2").style.top == "206px"){
+            $(".wrapper").style.transform = "translateX(-200%)"
+            $(".wrapper").style.transition = "transform .5s ease-in"
+            $(".success").style.transform = "translateX(0)"
+            $(".success").style.transition = "transform .5s ease-in"
+            finish.play()
+            $(".success-img").addEventListener("click", ()=> {
+              window.location.reload()
+            })
+          }
+        })()
+      })
 }, 700);
+
